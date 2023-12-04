df166df7a0069fe01f900c98a41f1907bd9cc302
Mashaun18 <animashaunoreoluwa@gmail.com>
2023-12-04 16:13:43 +0100

    fix modify admin file

--- Zion/Ria/admin.py
+++ Zion/Ria/admin.py
@@ -1,9 +1,24 @@
 from django.contrib import admin
 from .models import Product
+from .models import Cart
+from .models import CartItem
 
-# Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ["name", "price", "category"]
     list_filter = ["price", "name", "category"]
     search_fields = ["name", "category"]
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ["id", "date_created", "user"]
+    list_filter = ["user", "id", "date_created"]
+    search_fields = ["user", "id"]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ["cart", "product", "quantity"]
+    list_filter = ["product", "cart"]
+    search_fields = ["cart", "product"]
