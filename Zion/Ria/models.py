c9a70a90068d47341f93907aa9ec45a30c7f04ec
Mashaun18 <animashaunoreoluwa@gmail.com>
2023-12-05 14:07:32 +0100

    fix modify user file

--- Zion/Ria/models.py
+++ Zion/Ria/models.py
@@ -6,7 +6,7 @@ import uuid
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     category = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='Ria_images/', blank=True, null=True)
