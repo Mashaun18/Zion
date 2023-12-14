2c9e4733d0ffe8ae1026996b7a3fee4bbe1db827
Mashaun18 <animashaunoreoluwa@gmail.com>
2023-12-14 16:06:35 +0100

    commit new changes

--- Zion/Ria/urls.py
+++ Zion/Ria/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', views.products, name='products'),
     path('add_product/', views.add_product, name='add_product'),
     path('create_cart/', views.create_cart, name='create_cart'),
-    path('cart_items/', views.cart_items, name='cart_items'),
-    path('<uuid:id>/add_cart_item/', views.add_cart_item, name='add_cart_item'),
-    path('remove_cart_item/', views.remove_cart_item, name='remove_cart_item'),
-    path('<int:id>/', views.product, name='product'),
+    path('cart_items/<uuid:cart_id>/', views.cart_items, name='cart_items'),
+    path('remove_cart_item/<uuid:cart_id>/<uuid:product_id>/', views.remove_cart_item, name='remove_cart_item'),
+    path('<uuid:cart_id>/add_cart_item/', views.add_cart_item, name='add_cart_item'),
+    path('<uuid:id>/', views.product, name='product'),
 ]
--- Zion/Ria/views.py
+++ Zion/Ria/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .models import Product, Cart, CartItem
 from .serializers import ProductSerializer, CartSerializer, CartItemSerializer
-
+from rest_framework import status
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
@@ -54,11 +54,11 @@ def create_cart(request):
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def cart_items(request):
+def cart_items(request, cart_id):
     user = request.user
     try:
-        cart = Cart.objects.get(user=user)
-        cart_items = CartItem.objects.filter(cart=cart)
+        cart = Cart.objects.get(user=user, id=cart_id)
+        cart_items = CartItem.objects.filter(cart=cart_id)
         serializer = CartItemSerializer(cart_items, many=True)
         return Response(serializer.data)
     except Cart.DoesNotExist:
@@ -97,46 +97,31 @@ def cart_items(request):
 @permission_classes([IsAuthenticated])
 def add_cart_item(request, cart_id):
     current_cart = get_object_or_404(Cart, id=cart_id, user=request.user)
-    
-    data = request.data
-    product_name = data.get('product_name')
-    quantity = data.get('quantity', 1) 
-
-    try:
-        product = Product.objects.get(id=product_name)
-    except Product.DoesNotExist:
-        return Response({"detail": "Invalid product id"}, status=400)
-
-    if quantity <= 0:
-        return Response({"detail": "Quantity must be greater than 0"}, status=400)
-
-    cart_item, created = CartItem.objects.get_or_create(
-        cart=current_cart, product=product)
-   
-    cart_item.quantity += quantity
+    new_cartitem = CartItem(cart=current_cart)
+    cart_item = CartItemSerializer(new_cartitem, data=request.data)
+    cart_item.is_valid(raise_exception=True)
     cart_item.save()
-
-    return Response({"detail": "Cart item added successfully"})
+    return Response({"detail": "Cart item added successfully"}, status=status.HTTP_201_CREATED)
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def remove_cart_item(request):
-    data = request.data
+def remove_cart_item(request, product_id, cart_id):
+    # data = request.data
     user = request.user
     try:
         cart = Cart.objects.get(user=user)
     except Cart.DoesNotExist:
         return Response({"detail": "No cart available for this user"})
 
-    product_id = data.get('product_id')
+    # product_id = data.get('product_id')
     try:
         product = Product.objects.get(id=product_id)
     except Product.DoesNotExist:
         return Response({"detail": "Invalid product id"})
 
     try:
-        cart_item = CartItem.objects.get(cart=cart, product=product)
+        cart_item = CartItem.objects.get(cart=cart_id, product=product_id)
         cart_item.delete()
         return Response({"detail": "Cart item removed successfully"})
     except CartItem.DoesNotExist:
